bac06015348e73e2ea5eb256c2bd7625e83bcf27
Mstrmindz <tunjexi@yahoo.com>
2023-02-27 11:08:41 +0100

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,3 +1,5 @@
+//This function creates a new binary tree node with the given value and parent node, and sets the left and right child pointers to NULL. If the malloc() call fails to allocate memory for the new node, the function returns NULL to indicate the failure.
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
